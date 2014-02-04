d717a620763d570e1be4956a9f6a6bdfb561f5ff
Ryder Ross <ryross@gmail.com>
2014-02-04 11:37:40 -0800

    create a new finder instance using the injected Finder class so mocking works.

--- src/PragmaRX/Glottos/Repositories/DataRepository.php
+++ src/PragmaRX/Glottos/Repositories/DataRepository.php
@@ -324,7 +324,8 @@ class DataRepository implements DataRepositoryInterface {
 
 		$locale = Locale::make($locale);
 
-		foreach($this->finder->files()->in($path.'/'.$locale->getText()) as $file)
+		$finder = $this->finder->create();
+		foreach($finder->files()->in($path.'/'.$locale->getText()) as $file)
 		{
 			$values = $this->fileSystem->getRequire($file);
 
